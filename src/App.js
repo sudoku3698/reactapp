d908209b1f10ffb6b057b30daabd7a789d7f55b3
sudesh jadhav <sud.jad3698@gmail.com>
2021-04-13 11:05:51 +0530

    card details added

--- src/App.js
+++ src/App.js
@@ -6,17 +6,26 @@ import SignUp from './SignUp'
 import Login from './Login'
 import cakes from './cakedata';
 import {useState} from 'react';
+import CakeDetails from './CakeDetails'
 function App() {
 var [login,setLogin]=useState(false);
+var [show,setShow]=useState(false)
+var [com,setCom]=useState({});
+let showDetails=(data)=>{
+    setShow(true)
+    setCom(data)
+}
   return (
     <div>
       <Navbar islogin={login} setlogin={setLogin}/>
       <Carousel/>
       <Login islogin={login} setlogin={setLogin}/>
       {/* <center><SignUp/></center> */}
+      {show?<CakeDetails  cdata={com}/>:''}
       <div className="row">
+        
         {cakes.length>0 && cakes.map((each,index)=>{
-          return(<Card cake={each} key={index}/>)
+          return(<Card cake={each} showdetails={showDetails} cakedata={each} key={index}/>)
         })}
       
      </div>
